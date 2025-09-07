d0f1676eb7ac6a13f452bbd7e15943db48cc4885
snieper121 <mredozubovlist@gmail.com>
2025-09-07 07:03:31 +0300

    update finished6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
     mavenCentral()
     maven { url = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/") }
     maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots/") }
-    maven { url = uri("https://jitpack.io") }
+    maven { url = uri("https://jitpack.io") }  // Добавить JitPack для Sora Editor
   }
 }
 
