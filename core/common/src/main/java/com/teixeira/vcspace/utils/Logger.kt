8f4ef068314b7e31173c214a693b1a8f62545ca4
snieper121 <mredozubovlist@gmail.com>
2025-09-09 12:21:19 +0300

    update finished0

--- core/common/src/main/java/com/teixeira/vcspace/utils/Logger.kt
+++ core/common/src/main/java/com/teixeira/vcspace/utils/Logger.kt
@@ -1,22 +1,8 @@
-/*
- * This file is part of Simple Code Editor.
- *
- * Simple Code Editor is free software: you can redistribute it and/or modify it under the terms of
- * the GNU General Public License as published by the Free Software Foundation, either version 3 of
- * the License, or (at your option) any later version.
- *
- * Simple Code Editor is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
- * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along with Simple Code Editor.
- * If not, see <https://www.gnu.org/licenses/>.
- */
 package com.simple.codeeditor.utils
 
 import android.util.Log
-import com.simple.codeeditor.common.BuildConfig
 import com.simple.codeeditor.extensions.doIf
+import com.simple.codeeditor.core.common.BuildConfig
 import java.util.WeakHashMap
 
 class Logger private constructor(private val tag: String) {
